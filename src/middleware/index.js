947a669ba2981fefc51db0d354a0c2860ff22375
Nilanjan Haldar <nil.haldar@gmail.com>
2024-06-02 17:52:07 +0530

    some bug fixes

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,12 +1,11 @@
 import { checkNameForCategory } from "./category.js";
 import { validateProductData } from "./product.js";
-import { checkDuplicateUsernameAndEmail, checkRoles } from "./user.js";
+import { checkRoles } from "./user.js";
 import { verifyToken, isAdmin } from "./authjwt.js";
 
 export {
     checkNameForCategory,
     validateProductData,
-    checkDuplicateUsernameAndEmail,
     checkRoles,
     verifyToken,
     isAdmin,
